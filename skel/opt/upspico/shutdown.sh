e41fa2cd9002b8f6fee54f01ae9389e22e6e6f7d
VRocker <craigr3@hotmail.com>
2016-05-25 18:37:38 +0100

    Adapted the shutdown script to wait for recorder.bin to exit before initialising poweroff

--- skel/opt/upspico/shutdown.sh
+++ skel/opt/upspico/shutdown.sh
@@ -3,4 +3,17 @@
 # UPS Pico shutdown script
 #
 
+# Send a SIGTERM to the camera recorder
+kill `pidof recorder.bin` &
+echo "Waiting for recorder to exit..."
+while true ; do
+        PID=`ps cat | grep recorder.bin | grep -v grep`
+        if [ -z "$PID" ]; then
+		echo "Killed!"
+		break
+	fi
+	sleep 1
+done
+
+# Power off the system
 poweroff
